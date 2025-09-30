cdd4dcf494578582a005f7a878614922a1fb955c
coding-faster <berichmore@naver.com>
2025-09-30 22:53:03 +0900

    First

---
+++ src/main/java/com/example/ToyShop/controller/NoticeController.java
@@ -0,0 +1,17 @@
+package com.example.ToyShop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice")
+public class NoticeController {
+
+    //공지사항 리스트 페이지
+    @GetMapping({"","/"})
+    public String notice(){
+        return "notice/list";
+    }
+}
