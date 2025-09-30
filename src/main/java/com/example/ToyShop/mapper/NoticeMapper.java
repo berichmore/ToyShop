e5261634212698928ba7a0106e094776790ceb25
coding-faster <berichmore@naver.com>
2025-09-30 23:44:20 +0900

    setting secret

---
+++ src/main/java/com/example/ToyShop/mapper/NoticeMapper.java
@@ -0,0 +1,14 @@
+package com.example.ToyShop.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public class NoticeMapper {
+
+    //offset부터 size수 만큼 게시글 목록 조회
+
+    //전체 게시글 수 조회
+
+}
